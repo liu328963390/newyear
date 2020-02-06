e7cba0f1ef2ac3461715a0cf29dbe0b15e7aea1e
刘佳龙 <new2019from@163.com>
2020-02-06 22:41:17 +0800

    栈

---
+++ shirocontroll/src/main/java/com/shiro/news/shirocontroll/ShirocontrollApplication.java
@@ -0,0 +1,13 @@
+package com.shiro.news.shirocontroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirocontrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShirocontrollApplication.class, args);
+    }
+
+}
